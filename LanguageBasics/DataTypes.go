89a2e66dbad49f1e79c37a59646eab0be9dacac8
Manidhar Reddy <manidhar298@gmail.com>
2025-09-21 14:00:22 +0530

    basic data types in go

---
+++ LanguageBasics/DataTypes.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var flag bool = true
+	fmt.Println("bool flag ", flag)
+
+	var value int = 234
+	fmt.Println("Integer value : ", value)
+
+	var float float32 = 34.5
+
+	fmt.Println("Floating number : ", float)
+
+	var name string = "manidhar Reddy"
+
+	fmt.Println("Hello ", name)
+}
