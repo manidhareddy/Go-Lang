fde1bc896b22083f93f48f9b73431bcb51bc1635
Manidhar <manidhar298@gmail.com>
2025-11-19 23:21:42 +0530

    http package and json package usage along with io

---
+++ RESTAPI/go.mod
@@ -0,0 +1,3 @@
+module example.com/RESTAPI
+
+go 1.25.3
---
+++ RESTAPI/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"encoding/json"
+)
+
+const url = "https://dummyjson.com/products/categories"
+type ProductCategory struct {
+	Slug string `json:"slug"` // lowercase means private field |  uppercase means public field
+	Name string `json:"name"`
+	Url  string `json:"url"`
+}
+func main() {
+	response,error := http.Get(url)
+	if(error != nil){
+		fmt.Println("Error:", error)
+		panic(error)
+	}
+	defer response.Body.Close()
+	fmt.Println("Response Status Code:", response.StatusCode)
+	data,error := io.ReadAll(response.Body)
+	 if(error != nil){
+		fmt.Println("Error:", error)
+		panic(error)
+  }
+	if(response.StatusCode == http.StatusOK){
+		 var categories []ProductCategory
+		 fmt.Println("Data:", string(data))
+		error = json.Unmarshal(data, &categories)	
+		if(error != nil){
+			panic(error)
+		}	
+		for _, category := range categories {
+			fmt.Printf("%v\n", category.Name)
+		}
+	}else{
+		fmt.Println("Failed to fetch categories")
+	}
+}
