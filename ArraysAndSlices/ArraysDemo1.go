3b84e914a6b827afda9e47baecc119c21e16f5ad
Manidhar <manidhar298@gmail.com>
2026-06-21 18:27:47 +0530

    array demo

--- ArraysAndSlices/ArraysDemo1.go
+++ ArraysAndSlices/ArraysDemo1.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	grades := [2]int {34,45}
+	grades := [2]int{34, 45}
 
-	fmt.Printf("values : %v \n",grades);
+	fmt.Printf("values : %v \n", grades)
 
-	var values[2]int
-	fmt.Printf(" values  : %v \n",values)
+	var values [2]int
+	fmt.Printf(" values  : %v \n", values)
+
+	array := [5]int{2, 1, 3, 4}
+
+	fmt.Println(array)
 
 }
